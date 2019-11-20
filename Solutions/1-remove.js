67e0b902ef51c6a5df7d85665439b9f704ad41d7
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2019-11-20 23:43:56 +0200

    Rempve solutions

--- Solutions/1-remove.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const removeElement = (array, item) => {
-  const index = array.indexOf(item);
-  if (index !== -1) array.splice(index, 1);
-};
-
-module.exports = { removeElement };
