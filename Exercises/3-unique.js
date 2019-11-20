f10c8267f756beefe513f376f0a2adc523638438
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2019-11-20 23:28:34 +0200

    Add tasks

---
+++ Exercises/3-unique.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// Create and return a new array without duplicate elements
+// Don't modify initial array
+
+const unique = array => [];
+
+module.exports = { unique };
---
+++ Exercises/4-difference.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// Find difference of two arrays
+// elements from array1 that are not includes in array2
+
+const difference = (array1, array2) => [];
+
+module.exports = { difference };
