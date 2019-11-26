755f8b321a044aca621a51552579563693eca9e5
yehorbk <e.bublyk1010@gmail.com>
2019-11-26 14:22:33 +0200

    Another way of implementation

--- Exercises/4-difference.js
+++ Exercises/4-difference.js
@@ -3,6 +3,7 @@
 // Find difference of two arrays
 // elements from array1 that are not includes in array2
 
-const difference = (array1, array2) => [];
+const difference = (array1, array2) =>
+  array1.filter(e => !array2.includes(e));
 
 module.exports = { difference };
