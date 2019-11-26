83f978b98e77988d5a2ad9efab41c22033058e18
Niyaz Garipov <Niayz_Indicus@mail.ru>
2019-11-26 23:41:39 +0300

    Completes second exercise

--- Exercises/2-elements.js
+++ Exercises/2-elements.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const removeElements = (array, ...items) => {
-  // Remove multiple items from array modifying original array
+  for (const item of items) {
+    const index = array.indexOf(item);
+    index > -1 ? array.splice(index, 1) : array;
+  }
 };
 
 module.exports = { removeElements };
