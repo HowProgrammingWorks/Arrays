1af453d00554b59b6b77dd12017a69605c7568ea
Niyaz Garipov <Niayz_Indicus@mail.ru>
2019-11-27 08:38:11 +0300

    Completes third exercise

--- Exercises/3-unique.js
+++ Exercises/3-unique.js
@@ -1,8 +1,11 @@
 'use strict';
 
-// Create and return a new array without duplicate elements
-// Don't modify initial array
-
-const unique = array => [];
+const unique = array => {
+  const newArray = [];
+  for (const item of array) {
+    !newArray.includes(item) ? newArray.push(item) : array;
+  }
+  return newArray;
+};
 
 module.exports = { unique };
