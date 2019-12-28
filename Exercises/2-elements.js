b93429fbf660fbcb7e7819a0b7df4d692897209c
alexandr.pavletsov <alexandr.pavletsov@rcntec.com>
2019-12-28 20:04:40 +0500

    resize line length

--- Exercises/2-elements.js
+++ Exercises/2-elements.js
@@ -2,7 +2,9 @@
 // Remove multiple items from array modifying original array
 
 const removeElements = (array, ...items) => {
-  items.map(it => { if (array.includes(it)) array.splice(array.indexOf(it), 1); });
+  items.map(it => {
+    if (array.includes(it)) array.splice(array.indexOf(it), 1);
+  });
 };
 
 module.exports = { removeElements };
