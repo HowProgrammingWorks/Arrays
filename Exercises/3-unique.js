d0626b47463b90ec036dbbb2b3c97847edfe8e73
vladimirvikulin <v.vikulin.im11@kpi.ua>
2021-11-12 19:42:29 +0200

    Arrays

--- Exercises/3-unique.js
+++ Exercises/3-unique.js
@@ -1,8 +1,14 @@
 'use strict';
 
-// Create and return a new array without duplicate elements
-// Don't modify initial array
-
-const unique = (array) => [];
-
+const unique = (array) => {
+  const result = [];
+  for (const str of array) {
+    if (!result.includes(str)) {
+      result.push(str);
+    }
+  }
+  return result;
+};
+const result = unique([2, 1, 1, 3, 2]);
+console.log(result);
 module.exports = { unique };
--- Exercises/4-difference.js
+++ Exercises/4-difference.js
@@ -1,8 +1,15 @@
 'use strict';
 
-// Find difference of two arrays
-// elements from array1 that are not includes in array2
-
-const difference = (array1, array2) => [];
-
+const difference = (array1, array2) => {
+  const array = [];
+  for (let i = 0; i < array1.length; i++) {
+    const item = array1[i];
+    if (!array2.includes(item)) array.push(item);
+  }
+  return array;
+};
+const array1 = [7, -2, 10, 5, 0];
+const array2 = [0, 10];
+const result = difference(array1, array2);
+console.log(result);
 module.exports = { difference };
