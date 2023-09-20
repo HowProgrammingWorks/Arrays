24619a55422abd18a77e93ca0af8f6b734097fac
Jhon Mosk <jhon-mosk@yandex.ru>
2023-09-20 22:27:30 +0300

    labs done

--- Exercises/1-remove.js
+++ Exercises/1-remove.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const removeElement = (array, item) => {
-  // Remove item from array modifying original array
+  const index = array.indexOf(item);
+  if (index !== -1) array.splice(index, 1);
 };
 
 module.exports = { removeElement };
